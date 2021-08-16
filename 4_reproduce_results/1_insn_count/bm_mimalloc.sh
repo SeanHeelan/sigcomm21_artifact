e4b3f8187a48004bf03b7f9ae718c2f901c36070
Sean Heelan <sean@heelan.io>
2021-08-16 15:29:42 +0100

    Fix paths

--- 4_reproduce_results/1_insn_count/bm_mimalloc.sh
+++ 4_reproduce_results/1_insn_count/bm_mimalloc.sh
@@ -44,7 +44,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_redirect_err
 	echo "xdp_redirect_err... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}ebpf_samples/xdp_monitor_kern_tracepoint-xdp-xdp_redirect_err
-	output_dir="${output_prefix}/xdp_redirect_err/$x"
+	output_dir="${output_prefix}/xdp_redirect_err/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 5 --st_ex 0 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.33333333 --p_inst 0.33333333 --p_inst_as_nop 0.15 --reset_win_niter 50000 --win_s_list 12 --win_e_list 14 --path_res $output_dir -n 2500000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -54,7 +54,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp1_kern/xdp1
 	echo "xdp1_kern/xdp1... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}ebpf_samples/xdp1_kern_xdp1
-	output_dir="${output_prefix}/xdp1_kern-xdp1/$x"
+	output_dir="${output_prefix}/xdp1_kern-xdp1/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 1.5 --st_ex 0 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.33333333 --p_inst 0.33333333 --p_inst_as_nop 0.15 --reset_win_niter 80000 --win_s_list 56,56,5,48 --win_e_list 58,58,8,49 --path_res $output_dir -n 2500000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -64,7 +64,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_map_access
 	echo "xdp_map_access... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}simple_fw/xdp_map_access_kern_xdp_map_acces
-	output_dir="${output_prefix}/xdp_map_access/$x"
+	output_dir="${output_prefix}/xdp_map_access/$x/"
 	mkdir -p $output_dir
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 1.5 --st_ex 1 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.4 --p_inst 0.4 --p_inst_as_nop 0.15 --reset_win_niter 50000 --win_s_list 2,8 --win_e_list 5,9 --path_res $output_dir -n 5000000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -74,7 +74,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_devmap_xmit
 	echo "xdp_devmap_xmit... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}ebpf_samples/xdp_monitor_kern_tracepoint-xdp-xdp_devmap_xmit
-	output_dir="${output_prefix}/xdp_devmap_xmit/$x"
+	output_dir="${output_prefix}/xdp_devmap_xmit/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 1.5 --st_ex 1 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.33333333 --p_inst 0.33333333 --p_inst_as_nop 0.15 --reset_win_niter 80000 --win_s_list 15,31,12,21,26,1 --win_e_list 18,33,14,23,28,2 --path_res $output_dir -n 2250000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -84,7 +84,7 @@ for x in $(seq $repeat_cnt); do
 	# sys_enter_open
 	echo "sys_enter_open... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}ebpf_samples/syscall_tp_kern_tracepoint-syscalls-sys_enter_open
-	output_dir="${output_prefix}/sys_enter_open/$x"
+	output_dir="${output_prefix}/sys_enter_open/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 1.5 --st_ex 0 --st_eq 0 --st_avg 0 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.4 --p_inst 0.4 --p_inst_as_nop 0.15 --reset_win_niter 80000 --win_s_list 10,10,0 --win_e_list 12,12,3 --path_res $output_dir -n 300000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -94,7 +94,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_cpumap_enqueue
 	echo "xdp_cpumap_enqueue... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}ebpf_samples/xdp_monitor_kern_tracepoint-xdp-xdp_cpumap_enqueue
-	output_dir="${output_prefix}/xdp_cpumap_enqueue/$x"
+	output_dir="${output_prefix}/xdp_cpumap_enqueue/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 5 --st_ex 1 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.4 --p_inst 0.4 --p_inst_as_nop 0.15 --reset_win_niter 50000 --win_s_list 22,22,14,18 --win_e_list 24,24,16,20 --path_res $output_dir -n 2000000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -104,7 +104,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_fw
 	echo "xdp_fw... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}simple_fw/xdp_fw_kern_xdp_fw
-	output_dir="${output_prefix}/xdp_fw/$x"
+	output_dir="${output_prefix}/xdp_fw/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 5 --st_ex 0 --st_eq 0 --st_avg 0 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.33333333 --p_inst 0.33333333 --p_inst_as_nop 0.15 --reset_win_niter 50000 --win_s_list 10,2,52 --win_e_list 13,5,55 --path_res $output_dir -n 700000"
 	$k2_cmd > "${output_dir}/log.txt"
@@ -114,7 +114,7 @@ for x in $(seq $repeat_cnt); do
 	# xdp_pktcntr
 	echo "xdp_pktcntr... $x/$repeat_cnt"
 	bm_file=${bm_file_prefix}katran/xdp_pktcntr_xdp-pktcntr
-	output_dir="${output_prefix}/xdp_pktcntr/$x"
+	output_dir="${output_prefix}/xdp_pktcntr/$x/"
 	mkdir -p "$output_dir"
 	k2_cmd="$PRELOAD_PFX ./main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 0 --w_e 0.5 --w_p 1.5 --st_ex 0 --st_eq 0 --st_avg 1 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.4 --p_inst 0.4 --p_inst_as_nop 0.15 --reset_win_niter 80000 --win_s_list 17,17,0 --win_e_list 19,19,2 --path_res $output_dir -n 5000000"
 	$k2_cmd > "${output_dir}/log.txt"
